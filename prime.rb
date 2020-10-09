1fdc0a381a3b26f1a6345d478488b7c94e6482d0
angelo milien <angelomilienjoseph@gmail.com>
2020-10-08 20:50:01 -0400

    submitting for now

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
 # Add  code here!
+
+
+def prime?(num)
+    if num % 1 == 0 && num % num == 0  
+       true
+    elsif num.negative? %  1 == 0 && num.negative? % num.negative? == 0
+        true   
+    else    
+     false
+    end
+end
+
+
